1fb03ae761456e72858839d9f44697b2ee6f80f5
indigoparadox <indigo.repo@interfinitydynamics.info>
2020-12-22 20:17:29 -0500

    Added mqtt reactions.

--- misplay/misplay.py
+++ misplay/misplay.py
@@ -10,6 +10,8 @@ import random
 import logging
 import configparser
 import errno
+from paho.mqtt import client as mqtt_client
+import ssl
 
 WP_MAX_W = 100
 TIME_X = 120
@@ -46,7 +48,26 @@ class Misplay( object ):
         except FileExistsError as e:
             pass
 
+        self.mqtt = mqtt_client.Client(
+            self.config.get( 'ipc.mqtt', 'uid' ) )
+        #client.on_connect = mqtt_on_connect
+        self.mqtt.connect(
+            self.config.get( 'ipc.mqtt', 'host' ),
+            self.config.getint( 'ipc.mqtt', 'port' ) )
+        self.mqtt.subscribe( '{}/#'.format(
+            self.config.get( 'ipc.mqtt', 'topic' ) ) )
+        if self.config.getboolean( 'ipc.mqtt', 'ssl' ):
+            self.mqtt.tls_set(
+                self.config.get( 'ipc.mqtt', 'ca' ),
+                tls_version=ssl.PROTOCOL_TLSv1_2 )
+        self.mqtt.on_message = self.mqtt_receive
+        self.mqtt.loop_start()
+
         self.clear()
+
+    def mqtt_receive( self, client, userdata, message ):
+        logger = logging.getLogger( 'misplay.mqtt.receive' )
+        logger.info( str( message.payload.decode( 'utf-8' ) ) )
 
     def clear( self ):
         self.epd.init( self.epd.lut_full_update )
