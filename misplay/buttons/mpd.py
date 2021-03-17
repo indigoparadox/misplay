9d1754f8a88bab48a3e92dab796946205f5e221d
indigoparadox <indigo.repo@interfinitydynamics.info>
2021-03-17 13:57:45 -0400

    Don't use buttons class for now.

--- misplay/buttons/mpd.py
+++ misplay/buttons/mpd.py
@@ -1,42 +1,41 @@
+#!/usr/bin/env python3
 
 import logging
+import argparse
 
 import buttonshim # pylint: disable=import-error
 from mpd import MPDClient
 
-# TODO: Make this configurable.
+# TODO: Make configurable.
 SOCK_ADDR = '/var/run/mpd/socket'
 
-class MPDButtonHandler( object ):
+mpc = MPDClient()
+mpc.idletimeout = False
 
-    def __init__( self ):
-        self.mpc = MPDClient()
-        self.mpc.idletimeout = False
+@buttonshim.on_press( buttonshim.BUTTON_C )
+def voldown_handler( button, pressed ): # pylint: disable=unused-argument
+    logger = logging.getLogger( 'button.voldown' )
+    logger.debug( 'button pressed' )
+    mpstatus = mpd_command( mpc.status ) # pylint: disable=no-member
+    mpd_command( mpc.setvol, int( mpstatus['volume'] ) - 5 ) # pylint: disable=no-member
+    logger.info( 'volume is now %d', mpstatus['volume'] )
 
-    @buttonshim.on_press( buttonshim.BUTTON_C )
-    def voldown_handler( self, button, pressed ): # pylint: disable=unused-argument
-        logger = logging.getLogger( 'button.voldown' )
-        logger.debug( 'button pressed' )
-        mpstatus = self.mpd_command( self.mpc.status ) # pylint: disable=no-member
-        self.mpd_command( self.mpc.setvol, int( mpstatus['volume'] ) - 5 ) # pylint: disable=no-member
-        logger.info( 'volume is now %d', mpstatus['volume'] )
+@buttonshim.on_press( buttonshim.BUTTON_D )
+def volup_handler( button, pressed ): # pylint: disable=unused-argument
+    logger = logging.getLogger( 'button.volup' )
+    logger.debug( 'button pressed' )
+    mpstatus = mpd_command( mpc.status ) # pylint: disable=no-member
+    mpd_command( mpc.setvol, int( mpstatus['volume'] ) + 5  ) # pylint: disable=no-member
+    logger.info( 'volume is now %d', mpstatus['volume'] )
 
-    @buttonshim.on_press( buttonshim.BUTTON_D )
-    def volup_handler( self, button, pressed ): # pylint: disable=unused-argument
-        logger = logging.getLogger( 'button.volup' )
-        logger.debug( 'button pressed' )
-        mpstatus = self.mpd_command( self.mpc.status ) # pylint: disable=no-member
-        self.mpd_command( self.mpc.setvol, int( mpstatus['volume'] ) + 5  ) # pylint: disable=no-member
-        logger.info( 'volume is now %d', mpstatus['volume'] )
+@buttonshim.on_press( buttonshim.BUTTON_E )
+def play_handler( button, pressed ): # pylint: disable=unused-argument
+    logger = logging.getLogger( 'button.playpause' )
+    logger.debug( 'button pressed' )
+    mpd_command( mpc.pause ) # pylint: disable=no-member
 
-    @buttonshim.on_press( buttonshim.BUTTON_E )
-    def play_handler( self, button, pressed ): # pylint: disable=unused-argument
-        logger = logging.getLogger( 'button.playpause' )
-        logger.debug( 'button pressed' )
-        self.mpd_command( self.mpc.pause ) # pylint: disable=no-member
-
-    def mpd_command( self, command, *args ):
-        self.mpc.connect( SOCK_ADDR )
-        res = command( *args )
-        self.mpc.disconnect()
-        return res
+def mpd_command( command, *args ):
+    mpc.connect( SOCK_ADDR )
+    res = command( *args )
+    mpc.disconnect()
+    return res
